fc20f44ee51bcc9e95407a46ccbecbeb84f30628
Michael Austin <mpaustin1993@gmail.com>
2020-07-05 10:14:34 -0500

    remove Gem files

--- tests/FuelQuoteTest.php
+++ tests/FuelQuoteTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class FuelQuoteTest extends TestCase
 {
-    public function testFuelQuoteGallonsInput()
+    public function testFuelQuoteGallonsNumbervsTextInput()
     {
         $correctGallons = '40';
         $incorrectGallons = 'Forty';
@@ -14,5 +14,52 @@ class FuelQuoteTest extends TestCase
 
     }
 
-    
+    public function testFuelQuoteGallonsSingleDigitInput()
+    {
+        $correctGallons = '4';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons);
+    }
+
+    public function testFuelQuoteGallonsDoubleDigitInput()
+    {
+        $correctGallons = '40';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons);
+    }
+
+    public function testFuelQuoteGallonsTripleDigitInput()
+    {
+        $correctGallons = '400';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons);
+    }
+
+    public function testFuelQuoteGallonsFourDigitInput()
+    {
+        $correctGallons = '4000';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons);
+    }
+
+    public function testFuelQuoteGallonsCommasInput()
+    {
+        $correctGallons = '4,000';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons); //Fail
+    }
+
+    public function testFuelQuoteDecimalInput()
+    {
+        $correctGallons = '4000.00';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons); //Fail
+    }
+
+    public function testFuelQuoteDecimalAndCommasInput()
+    {
+        $correctGallons = '4,000.00';
+
+        $this->assertMatchesRegularExpression("/^[0-9]*$/", $correctGallons); //Fail
+    }
 }
--- tests/ManagePasswordTest.php
+++ tests/ManagePasswordTest.php
@@ -4,15 +4,36 @@ use PHPUnit\Framework\TestCase;
 
 class ManagePasswordTest extends TestCase
 {
-    public function testPasswordMatchInput()
+    public function testPasswordMatchNormalInput()
     {
 
         $password = 'passwordDemo123!';
-        $noMatchConfirmPwd = 'Demo123';
         $matchConfirmPwd = 'passwordDemo123!';
+        $noMatchConfirmPwd = 'Demo123';
 
-        // $this->assertEquals($password, $noMatchConfirmPwd);
         $this->assertEquals($password, $matchConfirmPwd);
-        $this->assertEquals($password, $noMatchConfirmPwd);
+        $this->assertEquals($password, $noMatchConfirmPwd); //Fail
+    }
+
+    public function testPasswordMatchCaseInput()
+    {
+
+        $password = 'passworddemo';
+        $matchConfirmPwd = 'passworddemo';
+        $noMatchConfirmPwd = 'PASSWORDDEMO';
+
+        $this->assertEquals($password, $matchConfirmPwd);
+        $this->assertEquals($password, $noMatchConfirmPwd); //Fail
+    }
+
+    public function testPasswordMatchSpaceInput()
+    {
+
+        $password = 'password demo';
+        $matchConfirmPwd = 'password demo';
+        $noMatchConfirmPwd = 'passworddemo';
+
+        $this->assertEquals($password, $matchConfirmPwd);
+        $this->assertEquals($password, $noMatchConfirmPwd); //Fail
     }
 }
